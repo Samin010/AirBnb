48252868bb8571cb9fad41685c8d634b541338e6
Samin010 <saminbashir484@gmail.com>
2024-11-30 20:09:59 +0530

    "test2"

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(userRouter)
 app.use(hostRouter);
 
 
+// Hello
+
 app.use(express.static(path.join(rootDir, "public")));
 app.use((req,res,next)=>{
  res.status(404).sendFile(path.join(rootDir,"views","404.html"))
